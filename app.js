4e5ed51f59d8754a9431174ebe7cdef30dd7d26c
Zoom <andrey.kropachev@lamoda.ru>
2022-08-26 23:12:42 +0300

    refactor

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb2', {
   useNewUrlParser: true,
 });
 
