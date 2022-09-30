2c8f8f9c6d5e1527205a4e20a63d85a92e3d19b5
Andrew K <nscapital08@gmail.com>
2022-10-01 01:56:35 +0300

    refactor

--- controllers/users.js
+++ controllers/users.js
@@ -40,7 +40,7 @@ module.exports.login = (req, res, next) => {
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
     .orFail(new NotFoundError('Пользователь не найден'))
-    .then((user) => res.send({ email: user.email, name: user.name }))
+    .then((user) => res.send({ email: user.email, name: user.name, userId: user._id }))
     .catch(next);
 };
 
